f4093396086ef13054bb0e9ee228ce6be11109d0
Amine derbal <aminedoran31@gmail.com>
2023-07-03 17:35:34 +0100

    Update groups controller

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -1,3 +1,39 @@
 class GroupsController < ApplicationController
-  def index; end
+  before_action :set_group, only: %i[ show edit update destroy ]
+  before_action :authenticate_user!
+  def index
+    @user = current_user
+    @groups = @user.groups
+  end
+
+  def new 
+    @group = Group.new
+  end
+
+  def create 
+    @group = Group.new(group_params.except(:icon))
+    @group.author = current_user
+    uploaded_icon = params[:group][:icon]
+    if uploaded_icon.present?
+      file_path = Rails.root.join('app', 'assets', 'images', uploaded_icon.original_filename)
+      File.open(file_path, 'wb') do |file|
+        file.write(uploaded_icon.read)
+      end
+      @group.icon = uploaded_icon.original_filename
+    end
+    if @group.save
+      redirect_to groups_path
+    else
+      render :new
+    end
+
+  end
+
+  private 
+
+  def group_params
+
+    params.require(:group).permit(:name, :icon)
+  end
+
 end
