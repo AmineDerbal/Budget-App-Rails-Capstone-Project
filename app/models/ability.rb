acc5e333226ab26edd334d57fb3f3725ca344c41
Amine derbal <aminedoran31@gmail.com>
2023-07-03 11:12:30 +0100

    config authorizations

---
+++ app/models/ability.rb
@@ -0,0 +1,16 @@
+# frozen_string_literal: true
+
+class Ability
+  include CanCan::Ability
+
+  def initialize(user)
+    user ||= User.new # guest user
+
+    return unless user.present?
+    can(:read,Group,:author_id=>user.id)
+    can(:read,Expense,:author_id=>user.id)
+    can(:read,User,:id=>user.id)
+
+
+  end
+end
