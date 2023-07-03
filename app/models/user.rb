e176a0965e90f12f79e6f9733fd686b48bdbbbcd
Amine derbal <aminedoran31@gmail.com>
2023-07-03 10:53:16 +0100

    Config devise parameters

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,8 @@
 class User < ApplicationRecord
+  # Include default devise modules. Others available are:
+  #  :lockable, :timeoutable, :trackable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable,:confirmable
 has_many :expenses,class_name: "Expense", foreign_key: "author_id"
 has_many :groups,class_name: "Group", foreign_key: "author_id"
 
