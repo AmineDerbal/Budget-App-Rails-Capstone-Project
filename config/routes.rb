4ab23efa92d8a2b070c8ac75c055e5803bfac872
Amine derbal <aminedoran31@gmail.com>
2023-07-04 09:57:41 +0100

    Add routing for expenses

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
   
   resources :home, only: :index
-  resources :groups, only: [:index, :new, :create],path: 'categories'
+  resources :groups, only: [:index, :new, :create],path: 'categories' do
+    resources :expenses, only: [:index, :new, :create] 
+  end
   devise_for :users
  
   # Add the following route for signing out
