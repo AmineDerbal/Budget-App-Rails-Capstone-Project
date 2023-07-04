d7f1c1a1d9c5af1afee00942660ae0ca868ad674
Amine derbal <aminedoran31@gmail.com>
2023-07-04 09:58:03 +0100

    Fix expense model

--- app/models/expense.rb
+++ app/models/expense.rb
@@ -1,9 +1,9 @@
 class Expense < ApplicationRecord
  belongs_to :author,class_name: "User", foreign_key: "author_id"
  has_many :expense_groups,class_name: "ExpenseGroup", foreign_key: "group_id", dependent: :destroy
- has_many :expenses, through: :expense_groups
+ has_many :groups, through: :expense_groups
 
- accepts__nested_attributes_for :expense_groups
+ accepts_nested_attributes_for :expense_groups
 
  validates :name, presence: true
  validates :amount, presence: true, numericality: {greater_than_or_equal_to: 0}
