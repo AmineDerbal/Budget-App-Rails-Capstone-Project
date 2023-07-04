faadcf8dfbc0520e3656ed58ce0fc62edf4d317c
Amine derbal <aminedoran31@gmail.com>
2023-07-04 17:12:33 +0100

    Update production config

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -63,7 +63,19 @@ Rails.application.configure do
   # config.active_job.queue_name_prefix = "budget_app_production"
 
   config.action_mailer.perform_caching = false
-  config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
+  config.action_mailer.default_url_options = { host: 'https://budget-expense-app.onrender.com' }
+
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+    address:              'smtp.gmail.com',
+    port:                 587,
+    domain:               'https://budget-expense-app.onrender.com',
+    user_name:            'aminedoran31@gmail.com',
+    password:             'biywwagyuptnmlgt',
+    authentication:       'plain',
+    enable_starttls_auto: true,
+    open_timeout:         5,
+    read_timeout:         5 }
 
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
